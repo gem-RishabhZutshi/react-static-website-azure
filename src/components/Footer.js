8a5fef2181ac5015e1d7befdd164e52e47942937
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-08-23 18:08:21 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,22 +17,22 @@
 
 
 
-import React from 'react';
+// import React from 'react';
 
-const Footer = () => {
-    const handleClick = () => {
-        // Add your click event logic here
-        window.location.href = '/https://www.youtube.com/'; // Replace with the URL you want to navigate to
-    };
+// const Footer = () => {
+//     const handleClick = () => {
+//         // Add your click event logic here
+//         window.location.href = '/https://www.youtube.com/'; // Replace with the URL you want to navigate to
+//     };
 
-    return (
-        <footer className="container-fluid text-center">
-            <div onClick={handleClick} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
-                THE WEBSITE 
-            </div>
-            <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p>
-        </footer>
-    );
-};
+//     return (
+//         <footer className="container-fluid text-center">
+//             <div onClick={handleClick} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+//                 THE WEBSITE 
+//             </div>
+//             <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p>
+//         </footer>
+//     );
+// };
 
-export default Footer;
+// export default Footer;
