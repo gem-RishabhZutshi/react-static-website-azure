e3b445a02a9739c27bdfe5c01de45ec853344b79
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-08-23 18:05:43 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,38 @@
+// import React from 'react';
+
+// const Footer = () => {
+//     return (
+//         <footer class="container-fluid text-center">
+//             <a href="#myPage" title="To Top">
+//                 <span class="glyphicon glyphicon-chevron-up"></span>
+//             </a>
+//             <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p>
+//         </footer>
+//     )
+// }
+
+// export default Footer;
+
+
+
+
+
 import React from 'react';
 
 const Footer = () => {
+    const handleClick = () => {
+        // Add your click event logic here
+        window.location.href = '/https://www.youtube.com/'; // Replace with the URL you want to navigate to
+    };
+
     return (
-        <footer class="container-fluid text-center">
-            <a href="#myPage" title="To Top">
-                <span class="glyphicon glyphicon-chevron-up"></span>
-            </a>
+        <footer className="container-fluid text-center">
+            <div onClick={handleClick} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                THE WEBSITE 
+            </div>
             <p>Theme Made By <a href="#" title="Visit w3schools">Abid Akram</a></p>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
